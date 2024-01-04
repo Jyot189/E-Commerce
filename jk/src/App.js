6a79cf431890633528c42795d4a71c596f43f190
Jyot189 <jyotkhant2002@gmail.com>
2024-01-04 11:30:37 +0530

    .

--- jk/src/App.js
+++ jk/src/App.js
@@ -8,7 +8,6 @@ import Home from './Pages/Home';
 import ProductList from './Pages/ProductList';
 import Product from './Pages/Product'
 import ItemState from './context/items/ItemState'
-import SendMail from '../../E-commerceAPI/sendMail'
 // import itemContext from './context/items/itemContext'
 
 const App = () => {
@@ -24,7 +23,6 @@ const App = () => {
         <Route path="/register" element={<Register/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/cart" element={<Cart/>}/>
-        <Route path="/SendMail" element={<SendMail/>}/>
       </Routes>
     </Router>
     </ItemState>
